3660a4e18f90f013e7c76f684a3c1348f3eeb4e7
Kate Novakivska <kate.nova14@gmail.com>
2020-12-07 00:48:46 +0200

    first adding

---
+++ RentSystem/Controllers/RegistrationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentSystem.Models;
+
+namespace RentSystem.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        RentContext context = ContextHolder.Context;
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Registered()
+        {
+            ViewBag.Id = Session["userId"].ToString();
+            ViewBag.Login = Session["userLogin"].ToString();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            // var context = ContextHolder.Context;
+            context.Users.Add(user);
+            context.SaveChanges();
+            Session["userId"] = user.User_Id;
+            Session["userLogin"] = user.Login;
+
+            return RedirectToAction("Registered");
+        }
+    }
+}
---
+++ RentSystem/Controllers/UserPageController.cs
@@ -0,0 +1,35 @@
+﻿using RentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentSystem.Controllers
+{
+    public class UserPageController : Controller
+    {
+        private RentContext context = ContextHolder.Context;
+
+        // GET: UserPage
+        public ActionResult Show()
+        {
+            var id = 2;
+            var users = context.Users;
+            User user = users
+                .ToList()
+                .FindLast(
+                delegate(User currentUser) {
+                    return currentUser.User_Id == id;
+                });
+            ViewBag.User = user;
+            List<Appartment> appartments = context.Appartments.ToList().FindAll(
+                delegate (Appartment appartment)
+                {
+                    return appartment.Landlord_Id == user.User_Id;
+                });
+            ViewBag.Appartments = appartments;
+            return View();
+        }
+    }
+}
